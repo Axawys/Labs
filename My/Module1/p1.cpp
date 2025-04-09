6ccd0d96a2b26cd111f9275c3ca63b9d4e8865ad
axawys <axawys@gmail.com>
2025-04-09 21:44:30 +0300

    Задания из книги

---
+++ My/Module1/p1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Натуральные числа от 1 до 10:\n| ";
+    for (int i=1;i<11;i++){
+        cout<<i<<" | ";
+    }
+    cout<<endl;
+    return 0;
+}
