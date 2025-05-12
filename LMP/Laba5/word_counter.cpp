794f0da513d0241a0736e5ec8a60f5ec6f9e6d07
axawys <popovdima20057@gmail.com>
2025-05-12 12:32:25 +0300

    Laba5 some changes

--- LMP/Laba5/word_counter.cpp
+++ LMP/Laba5/word_counter.cpp
@@ -1,37 +1,35 @@
 #include "word_counter.h"
 #include <fstream>
-#include <cctype>
+#include <sstream>
+#include <vector>
+#include <locale>
 
-bool is_alpha_russian(unsigned char c) {
-    // Проверяем русские буквы в UTF-8 (кириллица)
-    // или стандартные буквы ASCII
-    return (c >= 0xC0 && c <= 0xFF) || std::isalpha(c);
-}
+using namespace std;
 
 int count_short_words(const std::string& filename, int max_letters) {
-    std::ifstream infile(filename);
+    ifstream infile(filename);
     if (!infile) {
-        return -1; // Ошибка открытия файла
+        return -1;
     }
 
+    // Устанавливаем локаль для поддержки UTF-8
+    infile.imbue(locale("en_US.UTF-8"));
+    
     int count = 0;
-    int current_word_length = 0;
-    char c;
-
-    while (infile.get(c)) {
-        if (is_alpha_russian(c)) {
-            current_word_length++;
-        } else {
-            if (current_word_length > 0 && current_word_length <= max_letters) {
-                count++;
+    string word;
+    
+    while (infile >> word) {
+        // Считаем количество символов (а не байт)
+        int length = 0;
+        for (char c : word) {
+            if ((c & 0xC0) != 0x80) { // Подсчет только начальных байт UTF-8 символов
+                length++;
             }
-            current_word_length = 0;
         }
-    }
-
-    // Проверяем последнее слово в файле
-    if (current_word_length > 0 && current_word_length <= max_letters) {
-        count++;
+        
+        if (length <= max_letters) {
+            count++;
+        }
     }
 
     infile.close();
--- LMP/Laba5/word_counter.h
+++ LMP/Laba5/word_counter.h
@@ -4,10 +4,6 @@
 #include <string>
 #include <fstream>
 
-// Функция для подсчета слов с не более чем max_letters буквами
 int count_short_words(const std::string& filename, int max_letters = 4);
-
-// Вспомогательная функция для проверки, является ли символ буквой
-bool is_alpha_russian(unsigned char c);
 
 #endif // WORD_COUNTER_H
