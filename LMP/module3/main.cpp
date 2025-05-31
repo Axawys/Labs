444a69422141d74a2f83fc63e623966fdd3cef83
axawys <popovdima20057@gmail.com>
2025-05-31 12:45:54 +0300

    lmp module 3 minor update

--- LMP/module3/main.cpp
+++ LMP/module3/main.cpp
@@ -14,21 +14,21 @@ int main() {
 
     int rows, cols;
     input >> rows >> cols;
-    cout << "Размерность матрицы " << rows << " x " << cols << endl;
+    cout << "Размерность матрицы: " << rows << " x " << cols << endl;
 
     vector<vector<int>> matrix(rows, vector<int>(cols));
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
             input >> matrix[i][j];
         }
     }
     input.close();
 
     vector<int> min_elements;
-    for (int i = 0; i < rows; ++i) {
+    for (int i = 0; i < rows; i++) {
         int min_in_row = INT_MAX;
-        for (int j = 0; j < cols; ++j) {
+        for (int j = 0; j < cols; j++) {
             if (matrix[i][j] < min_in_row) {
                 min_in_row = matrix[i][j];
             }
