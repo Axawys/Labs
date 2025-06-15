ef71616dfa68fe730d2369d4e89c360ee0390fef
axawys <popovdima20057@gmail.com>
2025-06-15 13:56:35 +0300

    My p1 add sum

--- My/Module1/p1.cpp
+++ My/Module1/p1.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
+    int sum = 0;
     cout<<"Натуральные числа от 1 до 10:\n| ";
     for (int i=1;i<11;i++){
         cout<<i<<" | ";
+        sum+=i;
     }
-    cout<<endl;
+    cout<<endl<<"Сумма: "<<sum<<endl;
     return 0;
 }
